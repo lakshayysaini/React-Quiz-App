80b667fb77804260210690f7f92a2d3c283bbb37
lakshayysaini <lakshayysaini@gmail.com>
2023-05-14 11:49:23 +0530

    updated start, Questions, end and modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Modal = ({ show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">{children}</section>
+    </div>
+  );
+};
+
+export default Modal;
